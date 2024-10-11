b6552fd41ef1df492a9ebafe7d74d90be2757456
QuinnCuijpers <Quinn.Cuijpers@gmail.com>
2024-10-11 19:11:39 +0200

    added docstrings for FarmerGame.py

--- src/FarmerGame.py
+++ src/FarmerGame.py
@@ -5,10 +5,33 @@ from collections.abc import Iterable
 
 
 class FarmerGame:
+    """
+    A class to represent a farmer's game.
+
+    Attributes:
+        itemNames (tuple[str, ...]): The names of the items in the game.
+        badStates (set[State]): A set of states that are invalid.
+        source (State): The starting state of the game.
+        target (State): The target state of the game.
+    """
 
     def __init__(
         self, itemNames: tuple[str, ...], badStates: Optional[Set[State]] = None
     ) -> None:
+        """
+        Initializes a Farmer's Game with the provided items and optionally defines bad states.
+
+        This method sets up the game based on a tuple of `itemNames`, which represent the items involved in the game.
+        The first item in the tuple is always the item (Farmer), that is required to cross the river with any other items.
+        Additionally, a set of "bad" or forbidden states can be provided to specify configurations that are not allowed.
+
+        Args:
+            itemNames (tuple[str, ...]): A tuple of strings representing the names of the items in the game.
+                The first item in the tuple is the farmer, who must always accompany other items during crossings.
+            badStates (Optional[Set[State]], optional): A set of states that are considered illegal and should be avoided.
+                Defaults to None if no bad states are specified.
+        """
+
         self.itemNames = itemNames
         if not badStates:
             self.badStates: Set[State] = set()
@@ -30,17 +53,48 @@ class FarmerGame:
             self.target = target
 
     def addBadStates(self, badStates: Iterable[State]) -> None:
+        """
+        Adds all states in the given Iterable to `badStates`.
+
+        Args:
+            badStates (Iterable[State]): Iterable of states to be added to the `badStates` attribute of `FarmerGame`.
+        """
         for State in badStates:
             self.badStates.add(State)
 
-    def __backTrack(self, state: State) -> tuple[List[State], bool]:
-        path: List[State] = [state]
-        while state.prev:
-            path.append(state.prev)
-            state = state.prev
+    def __backTrack(self, endState: State) -> tuple[List[State], bool]:
+        """
+        Private method to compute a path to the specified end state by tracing back through its predecessors.
+
+        This method utilizes the `prev` attribute of `State` objects to reconstruct the sequence of states
+        leading to the given `endState`, effectively creating a path from the starting state to the target.
+
+        Args:
+            endState (State): The state for which the path is to be computed, based on its successive predecessors.
+
+        Returns:
+            tuple(List[State], bool): A tuple where the first element is a list of `State` objects representing the
+            path from the source to the target (if found), and the second element is a boolean indicating whether
+            the path was successfully found. If no path exists, the return value defaults to ([], False).
+        """
+
+        path: List[State] = [endState]
+        while endState.prev:
+            path.append(endState.prev)
+            endState = endState.prev
         return path[::-1], True
 
-    def __movesFromPath(self, path: List[State]) -> tuple[List[State], bool]:
+    def __movesFromPath(self, path: List[State]) -> None:
+        """
+        private method that returns the actions required to take for the given path using the following format:
+        f"Move {rightMovedItems} right" or f"Move {leftMovedItems} left".
+
+        Args:
+            path (List[State]): list of states representing a path.
+
+        Returns:
+            None: this method does not return any value.
+        """
         print(f"Actions required to reach State {self.target} from {self.source}:")
 
         for i in range(len(path) - 1):
@@ -65,9 +119,24 @@ class FarmerGame:
             elif rightMovedItems:
                 print(f"move {rightMovedItems} right")
 
-        return path, True
-
     def bfs(self, printActions: bool = False) -> tuple[List[State], bool]:
+        """
+        Performs a breadth-first search (BFS) to find a valid path from the source state to the target state.
+
+        Args:
+            printActions (bool, optional): If True, prints the sequence of actions required to go from the source state
+                to the target state. Defaults to False.
+
+        Raises:
+            ValueError: If the source state is not specified.
+            ValueError: If the target state is not specified.
+
+        Returns:
+            tuple(List[State], bool): A tuple where the first element is the list of states representing the path from
+            the source to the target (if found), and the second element is a boolean indicating whether the search
+            was successful. If no path is found, defaults to ([], False).
+
+        """
 
         if not isinstance(self.source, State):
             raise ValueError("Source is not specified")
@@ -84,7 +153,8 @@ class FarmerGame:
                 succes: bool
                 path, succes = self.__backTrack(curr)
                 if printActions:
-                    return self.__movesFromPath(path)
+                    self.__movesFromPath(path)
+                    return path, succes
                 else:
                     return path, succes
 
